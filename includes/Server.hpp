c365e29f46bee9cc23a54942cda15b7f860d1bb5
Nikita Androsov <androsov@Nikitas-MacBook-Air.local>
2024-08-03 21:18:49 +0200

    Testing socket connection

---
+++ includes/Server.hpp
@@ -0,0 +1,16 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <iostream>
+# include <unistd.h>
+
+class Server {
+public:
+	Server();
+	~Server();
+};
+
+#endif
